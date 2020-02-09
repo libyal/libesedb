48b1cd430cb2d92d79dd7d5d6950cbd656f1b7c7
Joachim Metz <joachim.metz@gmail.com>
2020-02-09 16:33:38 +0100

    Applied updates

--- tests/test_library.ps1
+++ tests/test_library.ps1
@@ -1,6 +1,6 @@
 # Tests C library functions and types.
 #
-# Version: 20181221
+# Version: 20200126
 
 $ExitSuccess = 0
 $ExitFailure = 1
@@ -41,7 +41,7 @@ Function GetTestToolDirectory
 {
 	$TestToolDirectory = ""
 
-	ForEach (${VSDirectory} in "msvscpp vs2008 vs2010 vs2012 vs2013 vs2015 vs2017" -split " ")
+	ForEach (${VSDirectory} in "msvscpp vs2008 vs2010 vs2012 vs2013 vs2015 vs2017 vs2019" -split " ")
 	{
 		ForEach (${VSConfiguration} in "Release VSDebug" -split " ")
 		{
