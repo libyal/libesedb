b5276483ba871d44b4de0d3418e2173dbf971e04
Joachim Metz <joachim.metz@gmail.com>
2023-01-18 06:19:16 +0100

    Applied updates

--- libesedb/libesedb_page_tree_key.c
+++ libesedb/libesedb_page_tree_key.c
@@ -1,7 +1,7 @@
 /*
  * Page tree key functions
  *
- * Copyright (C) 2009-2022, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2009-2023, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
